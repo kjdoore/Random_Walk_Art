1d4b172301369ff513c3dc378cfcd13e32605d6d
doorek <kjdoore@uark.edu>
2021-02-17 14:08:34 -0600

    Updated code and added README file explaining how code works

---
+++ README.md
@@ -0,0 +1,55 @@
+## Description ##
+A code to generate an unique .png image of a series of random walks constrained to a box. 
+Each walker is required to stay within the box region and cannot walk on locations 
+previously walked by other walkers. This allows for clear separation of colors in the 
+resulting image. Each walk is uniquely colored based on an input colormap, and therefore 
+generates an aesthetically pleasing image. 
+
+<img src="images/default.png" height=75/>  .
+
+## Installation ##
+------
+
+To install with Unix:
+```
+cd <install_dir>
+git clone https://github.com/kjdoore/Random_Walk_Art.git
+```
+
+
+## Execution ##
+------
+
+To run the code to generate an image, enter the following into a MATLAB command line:
+``
+addpath('<install_dir>/Random_Walk_Art/')
+random_walk_art
+``
+
+The following dialog box will open for inputs to be entered.
+<img src="images/dialog_box.png" height=75/>  .
+
+1. `Enter size of box region (x, y)`: two numbers separated by a comma or space. These 
+respective values give the x and y dimensions of the box that the walkers are constrained. 
+The larger the box is, the more area the walkers have and the long walkers will walk.
+*NOTE*: If this value is exceedingly large (total area > 250,000), then the code may take 
+several minutes to generate the image. 
+
+2. `Enter colormap name`: the name of the colormap to use for each walker. A list of native
+colormaps can be found [here](https://www.mathworks.com/help/matlab/ref/colormap.html). User
+defined colormaps can also be used if they are defined before running.
+
+3. `Invert color map (y or n)`: Inverts the colormap. Colors at the start of the colormap
+will likely cover larger areas of the image due to the walkers not being constrained by
+previous walkers.
+
+4. `Background color as normalized RGB triplet`: a normalized (max value of 1) RGB triplet
+containing the color of the background. Values must be separated by a space or comma.
+
+5. `Path of directory to save image`: the path to the directory that the image will be 
+saved.
+
+6. `Resolution of image in dpi`: the resolution of the .png image in dpi (dots per inch). 
+High values will result in high resolution images. *NOTE*: Large values (>1000) can 
+require several minutes to save the image.
+
--- random_walk_art.m
+++ random_walk_art.m
@@ -1,110 +1,129 @@
 % Allow user to input size of image, color map, directory to save image
 close all
-clearvars
+clear all
 
-prompt = {'Enter size of image (x, y):',...
-    'Enter colormap name (See https://www.mathworks.com/help/matlab/ref/colormap.html for list):',...
-    'Path of directory to save image:'};
+prompt = {'Enter size of box region (x, y)',...
+    'Enter colormap name (See https://www.mathworks.com/help/matlab/ref/colormap.html for list)',...
+    'Invert color map (y or n)',...
+    'Background color as normalized RGB triplet',...
+    'Path of directory to save image',...
+    'Resolution of image in dpi'};
 dlgtitle = 'Required Inputs';
 dims = [1 67];
-definput = {'80, 60','hsv','~/Desktop/'};
+definput = {'80, 60','hsv','n','0 0 0','~/Desktop/','300'};
 answer = inputdlg(prompt,dlgtitle,dims,definput);
 
-axis_dim=flip(str2num(answer{1}));
-color_map=answer{2};
-cm=colormap(color_map);
-dir=answer{3};
+axis_dim = str2num(answer{1});
+color_map = answer{2};
+if isequal(answer{3},'y')
+    cm = flipud(colormap(color_map));
+else
+    cm = colormap(color_map);
+end
+backgrd_color=str2num(answer{4});
+dir = answer{5};
+res = answer{6};
 
 clearvars dlgtitle dims definput prompt answer
 
+fig=figure(1);
 
-% Begin random walk
+
 % Walkers will start away from an edge and will stop once they reach an
 %   edge. They will also not be allowed to walk on a location that has
 %   previously been walked by a different walker.
-
-area_of_image=axis_dim(1)*axis_dim(2);
-max_steps=area_of_image;
-max_walkers=ceil(sqrt(area_of_image));
-walker=1;
-temp=1;
-cm_num=1;
+area_of_image = axis_dim(1)*axis_dim(2);
+max_walkers = min([ceil(sqrt(area_of_image)), 64]);
+max_steps = area_of_image/max_walkers*ceil(log10(area_of_image));
+walker = 1;
+cm_num = 1;
 if max_walkers < length(cm)
-    cm_scale=length(cm)/max_walkers;
+    cm_scale = length(cm)/max_walkers;
 else
-    cm_scale=1;
+    cm_scale = 1;
 end
-step=[[-1 -1];[0 -1];[1 -1];[-1 0];[0 0];[1 0];[-1 1];[0 1];[1 1]];
+
+% Possible new locations surrounding the current location
+% Do not allow for diagonal steps later, but here for convience
+step = [[-1 -1];[0 -1];[1 -1];[-1 0];[0 0];[1 0];[-1 1];[0 1];[1 1]];
 
 
 % Add two to each dimension for ease in calculation. True walking region is
 %   centered in the matrix (i.e., one extra column and row at both ends).
-walker_matrix=zeros(axis_dim+2);
-for i=1:max_walkers
+walker_matrix = zeros(axis_dim+2);
+walker_matrix(:,1)=-1;
+walker_matrix(1,:)=-1;
+walker_matrix(:,axis_dim(2)+2)=-1;
+walker_matrix(axis_dim(1)+2,:)=-1;
+
+% Begin random walk
+for i = 1:max_walkers
+    
     % Each walker must start away from the edge at a location not having
     %   already been walked
-    start=[ceil(rand*(axis_dim(1)-3))+3,ceil(rand*(axis_dim(2)-3))+3];
-    while walker_matrix(start(1),start(2)) ~= 0
-        start=[ceil(rand*(axis_dim(1)-3))+3,ceil(rand*(axis_dim(2)-3))+3];
-    end
-    current_loc=start;
-    walking_path=current_loc;
+    starting_loc=find(walker_matrix == 0);
+    [startx,starty]=ind2sub(size(walker_matrix),starting_loc(ceil(rand*length(starting_loc))));
+    start=[startx,starty];
+    current_loc = start;
+    walking_path = current_loc;
     
     % Walker will now walk till they can take no more steps or reach an
     %   edge
-    for j=1:max_steps
+    for j = 1:max_steps
         
         % Select neighboring locations that the walker can travel to speed
         %   up process. Also do not allow walker to stand still.
-        neighbor_loc=find(walker_matrix(current_loc(1)-1:current_loc(1)+1,current_loc(2)-1:current_loc(2)+1) == 0);
-        neighbor_loc=neighbor_loc(neighbor_loc ~= 5);
+        neighbor_loc = find(walker_matrix(current_loc(1)-1:current_loc(1)+1,current_loc(2)-1:current_loc(2)+1) == 0);
+        neighbor_loc = neighbor_loc(mod(neighbor_loc,2) == 0);
         
+        % If at an edge, do not allow to walk out of walking region
+        if current_loc(1)-1 == 1
+            neighbor_loc = neighbor_loc(neighbor_loc ~= 4);
+        end
+        if current_loc(2)-1 == 1
+            neighbor_loc = neighbor_loc(neighbor_loc ~= 2);
+        end
+        if current_loc(1)+1 == axis_dim(1)+2
+            neighbor_loc = neighbor_loc(neighbor_loc ~= 6);
+        end
+        if current_loc(2)+1 == axis_dim(2)+2
+            neighbor_loc = neighbor_loc(neighbor_loc ~= 8);
+        end
+        
+        % If the walker cannot take a step, then end their walk
         if isempty(neighbor_loc) 
             break
         end
         
-        % Pick one of the open neighboring cells to step to.
-        step_loc=ceil(rand*length(neighbor_loc));
-        new_loc=current_loc+step(neighbor_loc(step_loc),:);
-        
+        % Pick one of the open neighboring cells and step to it.
+        step_loc = ceil(rand * length(neighbor_loc));
+        new_loc = current_loc + step(neighbor_loc(step_loc),:);
+         
         % Stop the walker if they exit the walking region
-        if new_loc(1) < 2 || new_loc(2) < 2 || new_loc(1) > axis_dim(1)-1 || new_loc(2) > axis_dim(2)-1
-            break
-        end
+%         if new_loc(1) < 2 || new_loc(2) < 2 || new_loc(1) > axis_dim(1)-1 || new_loc(2) > axis_dim(2)-1
+%             bread
+%         end
         
-%         % If the walker steps onto a previously walked space by a different
-%         %   walker, then try stepping somewhere else.
-%         while walker_matrix(new_loc(1),new_loc(2)) ~= 0
-%             step_loc=ceil(rand*length(neighbor_loc));
-%             new_loc=current_loc+step(neighbor_loc(step_loc),:);
-%             if new_loc(1) < 1 || new_loc(2) < 1 || new_loc(1) > axis_dim(1) || new_loc(2) > axis_dim(2)
-%                 break
-%             end
-%         end
-%         if new_loc(1) < 1 || new_loc(2) < 1 || new_loc(1) > axis_dim(1) || new_loc(2) > axis_dim(2)
-%             break
-%         end
-        current_loc=new_loc;
-        walking_path=[walking_path;current_loc];
+        current_loc = new_loc;
+        walking_path = [walking_path; current_loc];
     end
     
-    for j=1:size(walking_path,1)
-        walker_matrix(walking_path(j,1),walking_path(j,2))=walker;
+    % Record locations walker has walked
+    for j = 1:size(walking_path,1)
+        walker_matrix(walking_path(j,1),walking_path(j,2)) = walker;
     end
     
     % Plot the walked path
     hold on
     plot(walking_path(:,1),walking_path(:,2),'Color',cm(ceil(cm_num*cm_scale),:))
-    cm_num=cm_num+1;
-    if mod(i,length(cm)) == 0
-        cm_num=1;
-    end
+    cm_num = cm_num + 1;
     
-    walker=walker+1;
+    walker = walker + 1;
     clearvars walking_path
 end
 
 % Clean up figure layout for saving
-set(gca,'XColor', 'none','YColor','none','Position',[0 0 1 1],'Xlim',[1 axis_dim(1)],'Ylim',[1 axis_dim(2)])
-%set(gcf, 'Position',  [100, 100, 2000, 1500])
-saveas(gcf,[dir,'Random_walk_art.png'])
+resolution=['-r',res];
+set(gca,'XColor', 'none','YColor','none','Position',[0 0 1 1],'Xlim',[1 axis_dim(1)+2],'Ylim',[1 axis_dim(2)+2],'Color',backgrd_color)
+set(gcf,'Color',backgrd_color,'InvertHardCopy', 'off')
+print(fig,[dir,'Random_walk_art.png'],'-dpng',resolution)
